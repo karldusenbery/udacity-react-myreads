0d910884b7062f7582a418b0a1a6c356a508cf8a
Karl Dusenbery <jkdusenbery@redfoundry.com>
2022-04-25 22:13:26 -0500

    Moved Search component.
    Moved Search component from App.js to Search.js.
    
    Also set up the SearchButton.js.
    
    TODO: fix back navigation.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Shelves from './components/Shelves'
+import Search from './components/Search'
 import { Route } from 'react-router-dom'
 
 class BooksApp extends React.Component {
@@ -19,27 +20,8 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          // This is the Search component
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          // This is where the Search component is accessed.
+          <Search/>
         ) : (
           <div className="list-books">
             {/*  This is the Header component */}
---
+++ src/components/SearchButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class SearchButton extends React.Component {
+
+    render() {
+        return (
+            
+        )
+    }
+}
